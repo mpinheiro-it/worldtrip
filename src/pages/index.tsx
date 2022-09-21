ac2416a94f84425ee91b33b5e3dc6356e59fb71e
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-09-20 21:16:53 -0300

    home screen for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Img, Box, Text } from '@chakra-ui/react'
+import { Flex, Img, Box, Text, Divider  } from '@chakra-ui/react'
 
 
 export default function Home() {
@@ -11,10 +11,54 @@ export default function Home() {
 
       
 
-      <Box w="100vw" h="150px" backgroundImage="url('..\..\logo.png')" bg={"lightblue"}>
-        <Text >5 Continentes,infinitas possibilidades.</Text>
-        <Text>Chegou a hora de tirar do papel a viagem que você sempre sonhou.</Text>
+      <Box w="100vw" h="40" background={"url('../../background.png')"}>
+        <Text 
+          mt={4}
+          ml={5}
+          color="#F5F8FA" 
+          fontSize={"2xl"}
+          fontWeight={"500"}
+          >
+            5 Continentes, <br/>
+         infinitas possibilidades.
+        </Text>
+        
+        <Text 
+          color="#DADADA"
+          mt={1} 
+          ml={5}
+          fontWeight={"400"} >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+        </Text>      
+      </Box>
+
       
+
+      <Box w={"80vw"}mt={10} color={"#47585B"} fontSize={"2xl"} fontWeight={"500"}>
+        
+        <Flex alignItems={"center"} mb={6}>
+          <Img src='..\..\ellipse.png' w={2} h={2} mr={3} /><Text mr={20}>vida noturna </Text>
+          <Img src='..\..\ellipse.png' w={2} h={2} mr={3} /><Text>praia</Text>
+        </Flex>
+
+        <Flex alignItems={"center"} mb={6}>
+          <Img src='..\..\ellipse.png' w={2} h={2} mr={3} /><Text mr={20}>moderno </Text>
+          <Img src='..\..\ellipse.png' w={2} h={2} mr={3} /><Text>clássico</Text>
+        </Flex>
+        
+       <Flex alignItems={"center"} justifyContent={"center"} mb={8}>
+        <Img src='..\..\ellipse.png' w={2} h={2} mr={3} /><Text>e mais...</Text>
+       </Flex>
+
+        <Flex alignItems={"center"} justifyContent={"center"} flexDir={"column"}>
+          <Divider borderWidth={"1px"} borderStyle={"solid"} borderColor={"black"} w={20} mb={8}  />          
+          <Text>Vamos nessa? </Text>
+          <Text>Então escolha seu continente </Text>
+        </Flex>
+       
+
+       
+       
       </Box>
     </Flex>       
     
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+    fonts: {
+        heading: `'Poppins', sans-serif`,
+        body: `'Poppins', sans-serif`,
+      },
+})
+
+
