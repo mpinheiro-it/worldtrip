6e4527cd3010ffa88819c76a19828baaac7c91a6
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-09-21 21:01:10 -0300

    creating carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import { Flex, Img, Box, Text, Divider  } from '@chakra-ui/react'
+import 'swiper/css';
+import 'swiper/css/bundle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination,  Scrollbar, A11y } from 'swiper';
 
 
 export default function Home() {
   return (
    
+  //Top Logo and flex that involves all items
     <Flex w="100vw" justify={"center"} alignItems={"center"} flexDir="column"> 
       <Box>
         <Img src='..\..\logo.png' alt='Logo' m={4} w={100} />
       </Box> 
 
       
-
+  
       <Box w="100vw" h="40" background={"url('../../background.png')"}>
         <Text 
           mt={4}
@@ -54,12 +59,31 @@ export default function Home() {
           <Divider borderWidth={"1px"} borderStyle={"solid"} borderColor={"black"} w={20} mb={8}  />          
           <Text>Vamos nessa? </Text>
           <Text>Então escolha seu continente </Text>
-        </Flex>
-       
-
-       
-       
+        </Flex>      
       </Box>
+
+
+      <Box w="100vw" h="400px" mt={5} >
+          <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              navigation 
+              pagination  
+              className="mySwiper"
+              slidesPerView={1}      
+              scrollbar={{ draggable: true }}
+              onSlideChange={() => console.log('slide change')}
+              onSwiper={(swiper) => console.log(swiper)}
+              
+              
+            >
+              <SwiperSlide> Europa <Img src='..\..\europe.png' w="100vw" /> </SwiperSlide>
+              <SwiperSlide> <Img src='..\..\asia.jpg' w="100vw" /> </SwiperSlide>
+              <SwiperSlide> <Img src='..\..\africa.jpg' w="100vw" /> </SwiperSlide>
+              <SwiperSlide> <Img src='..\..\oceania.jpg' w="100vw" /> </SwiperSlide>
+              <SwiperSlide> <Img src='..\..\america.jpg' w="100vw" /> </SwiperSlide>
+              
+          </Swiper>
+       </Box> 
     </Flex>       
     
     )
